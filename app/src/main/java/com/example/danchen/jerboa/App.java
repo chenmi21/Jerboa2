835445e053cb7d7d4cb20addf5c1452537f9d5f2
Tiffany Huang <queenian415@gmail.com>
2015-09-01 15:11:40 +0800

    Setup LeanCloud (Chinese Parse = =)

---
+++ app/src/main/java/com/example/danchen/jerboa/App.java
@@ -0,0 +1,24 @@
+package com.example.danchen.jerboa;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.avos.avoscloud.AVOSCloud;
+import com.avos.avoscloud.AVObject;
+
+/**
+ * Created by Tiffanie on 15-08-31.
+ */
+public class App extends Application {
+
+    public void onCreate() {
+        //如果使用美国节点，请加上这行代码 AVOSCloud.useAVCloudUS();
+        AVOSCloud.initialize(this, "NodF24j2nk6OdsvLj7hiWEyX", "yroBY6xCj1ND2wyDWCqBHm6z");
+
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+
+}
